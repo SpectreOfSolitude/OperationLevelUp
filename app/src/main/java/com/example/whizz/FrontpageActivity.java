21b9ec75d30e7704e652b7370dc13bb4e0663a49
Ahmad Al <ashabuljannah201@gmail.com>
2023-05-18 14:21:08 +0700

    intent_link_signup

--- app/src/main/java/com/example/whizz/FrontpageActivity.java
+++ app/src/main/java/com/example/whizz/FrontpageActivity.java
@@ -2,13 +2,29 @@ package com.example.whizz;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.whizz.ui.ui.login.LoginActivity;
 
 public class FrontpageActivity extends AppCompatActivity {
-
+    TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_frontpage);
+
+        textView=(TextView)findViewById(R.id.button_front);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(FrontpageActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
